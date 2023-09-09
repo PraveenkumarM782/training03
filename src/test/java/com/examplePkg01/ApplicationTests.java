89a1e578b8bf291616a9654f21a0fc2b5096e76f
praveen <youexample.com>
2023-09-09 13:12:45 +0530

    first demo projecct

---
+++ src/test/java/com/examplePkg01/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplePkg01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
